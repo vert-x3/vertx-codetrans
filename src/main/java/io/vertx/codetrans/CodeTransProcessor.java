680d914535e6cfc7137911559d77a6a45fdb5e5d
Julien Viet <julien@julienviet.com>
2015-03-12 16:36:30 +0100

    Copy resources in the target dir

--- src/main/java/io/vertx/codetrans/CodeTransProcessor.java
+++ src/main/java/io/vertx/codetrans/CodeTransProcessor.java
@@ -11,14 +11,23 @@ import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.TypeMirror;
+import javax.tools.FileObject;
+import javax.tools.StandardLocation;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URL;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -40,6 +49,7 @@ public class CodeTransProcessor extends AbstractProcessor {
   private File outputDir;
   private CodeTranslator translator;
   private List<Lang> langs;
+  private Set<File> folders = new HashSet<>(); // The copied folders so we don't do the job twice
 
   @Override
   public Set<String> getSupportedOptions() {
@@ -74,21 +84,56 @@ public class CodeTransProcessor extends AbstractProcessor {
               modifiers.contains(Modifier.PUBLIC) &&
               processingEnv.getTypeUtils().isSubtype(rootElt.asType(), verticleType)) {
             TypeElement typeElt = (TypeElement) rootElt;
+
+            //
+            String folderName = processingEnv.getElementUtils().getPackageOf(typeElt).getSimpleName().toString();
+            File dstFolder = new File(outputDir, folderName);
+            if (!dstFolder.exists()) {
+              dstFolder.mkdirs();
+            }
+
+            // Copy files
+            FileObject obj = processingEnv.getFiler().getResource(StandardLocation.SOURCE_PATH, "", typeElt.getQualifiedName().toString().replace('.', '/') + ".java");
+            File srcFolder = new File(obj.toUri()).getParentFile();
+            if (!folders.contains(srcFolder)) {
+              folders.add(srcFolder);
+              Path srcPath = srcFolder.toPath();
+              Path dstPath = dstFolder.toPath();
+              SimpleFileVisitor copyingVisitor = new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                  Path targetPath = dstPath.resolve(srcPath.relativize(dir));
+                  if(!Files.exists(targetPath)){
+                    log.println("Creating dir " + targetPath);
+                    Files.createDirectory(targetPath);
+                  }
+                  return FileVisitResult.CONTINUE;
+                }
+                @Override
+                public FileVisitResult visitFile(Path srcFile, BasicFileAttributes attrs) throws IOException {
+                  if (!srcFile.getFileName().toString().endsWith(".java")) {
+                    log.println("Copying resource " + srcFile + " to " + dstPath);
+                    Path dstFile = dstPath.resolve(srcPath.relativize(srcFile));
+                    Files.copy(srcFile, dstFile, StandardCopyOption.REPLACE_EXISTING);
+                  }
+                  return FileVisitResult.CONTINUE;
+                }
+              };
+              Files.walkFileTree(srcPath, copyingVisitor);
+            }
+
+            // Generate code
             for (Element enclosedElt : typeElt.getEnclosedElements()) {
               if (enclosedElt.getKind() == ElementKind.METHOD) {
                 ExecutableElement methodElt = (ExecutableElement) enclosedElt;
                 if (methodElt.getSimpleName().toString().equals("start") && methodElt.getParameters().isEmpty()) {
-                  String folderName = processingEnv.getElementUtils().getPackageOf(typeElt).getSimpleName().toString();
                   String fileName = typeElt.getSimpleName().toString().toLowerCase();
                   for (Lang lang : langs) {
                     try {
                       String translation = translator.translate(methodElt, lang);
-                      File folder = new File(outputDir, folderName);
-                      if (folder.exists() || folder.mkdirs()) {
-                        File f = new File(folder, fileName + "." + lang.getExtension());
-                        Files.write(f.toPath(), translation.getBytes(), StandardOpenOption.CREATE);
-                        log.println("Generated " + f.getAbsolutePath());
-                      }
+                      File f = new File(dstFolder, fileName + "." + lang.getExtension());
+                      Files.write(f.toPath(), translation.getBytes(), StandardOpenOption.CREATE);
+                      log.println("Generated " + f.getAbsolutePath());
                     } catch (Exception e) {
                       log.print("Skipping generation of " + typeElt.getQualifiedName());
                       e.printStackTrace(log);
