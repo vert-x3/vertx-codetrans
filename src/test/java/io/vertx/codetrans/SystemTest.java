4c6b05524a44f93d7b034c64f0c5caffcd9afd9f
Julien Viet <julien@julienviet.com>
2015-06-09 14:33:05 +0200

    Improve System.out.println test

--- src/test/java/io/vertx/codetrans/SystemTest.java
+++ src/test/java/io/vertx/codetrans/SystemTest.java
@@ -2,6 +2,11 @@ package io.vertx.codetrans;
 
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
 /**
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
  */
@@ -9,8 +14,18 @@ public class SystemTest extends ConversionTestBase {
 
   @Test
   public void testOutPrintln() {
-    runAll("system/Out", "println", () -> {
-      // We don't really test it uses the correct stuff but well...
-    });
+    PrintStream prevOut = System.out;
+    ByteArrayOutputStream writer = new ByteArrayOutputStream();
+    try {
+      PrintStream nextOut = new PrintStream(writer);
+      System.setOut(nextOut);
+      runAll("system/Out", "println", () -> {
+        nextOut.flush();
+        assertEquals("hello" + System.getProperty("line.separator"), writer.toString());
+        writer.reset();
+      });
+    } finally {
+      System.setOut(prevOut);
+    }
   }
 }
