b6b6d4fd44de59883bf76b8056f023201c7ee3bd
Julien Viet <julien@julienviet.com>
2016-10-07 22:25:45 +0200

    Smarter comparison of string in LiteralExpressionTest#testLiteralString - fixes #9

--- src/test/java/io/vertx/codetrans/LiteralExpressionTest.java
+++ src/test/java/io/vertx/codetrans/LiteralExpressionTest.java
@@ -73,8 +73,63 @@ public class LiteralExpressionTest extends ConversionTestBase {
     for (Lang lang : langs()) {
       CodeWriter writer = lang.codeBuilder().newWriter();
       writer.renderChars(expected);
-      assertEquals("\\n\\r\\t\\f\\b\\\"\\\\'\\u0000A\\u007F", writer.getBuffer().toString());
+      String ret = unescape(writer.getBuffer().toString());
+      assertEquals("\n\r\t\f\b\"\\'\u0000A\u007F", ret);
     }
+  }
+
+  private String unescape(String s) {
+    StringBuilder sb = new StringBuilder();
+    int len = s.length();
+    int i = 0;
+    while (i < len) {
+      char c = s.charAt(i++);
+      if (c == '\\') {
+        if (i < len) {
+          c = s.charAt(i++);
+          switch (c) {
+            case 'n':
+              sb.append('\n');
+              break;
+            case 'r':
+              sb.append('\r');
+              break;
+            case 't':
+              sb.append('\t');
+              break;
+            case 'f':
+              sb.append('\f');
+              break;
+            case 'b':
+              sb.append('\b');
+              break;
+            case '"':
+              sb.append('"');
+              break;
+            case '\\':
+              sb.append('\\');
+              break;
+            case 'u':
+              String sub;
+              try {
+                sub = s.substring(i, i += 4);
+              } catch (StringIndexOutOfBoundsException e) {
+                throw new IllegalArgumentException(e.getMessage());
+              }
+              int val = Integer.parseInt(sub, 16);
+              sb.append((char)val);
+              break;
+            default:
+              throw new UnsupportedOperationException("Handle me gracefully " + c);
+          }
+        } else {
+          sb.append('\\');
+        }
+      } else {
+        sb.append(c);
+      }
+    }
+    return sb.toString();
   }
 
   @Test
